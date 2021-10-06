832c5341242da3f36f01b4b4e968cd52f3cc11df
Alassane NIANG <adniang75@gmail.com>
2021-10-06 16:02:13 +0200

    Added placeholder for landing page

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,18 +1,12 @@
-package com.devopsbuddy;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-
+public class IndexController {
     @RequestMapping( "/" )
-    public String sayHello () {
+    private String home () {
         return "index";
-    }
-
-    @RequestMapping("/about")
-    public String about() {
-        return "about";
     }
 }
